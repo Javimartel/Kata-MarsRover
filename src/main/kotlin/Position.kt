b53d7f1198a2e57b0f39a27eed5c5dce7ef9895e
Javi <javiermespino@gmail.com>
2023-04-19 12:17:51 +0000

    Test(refactor): check if position is not negative

--- src/main/kotlin/Position.kt
+++ src/main/kotlin/Position.kt
@@ -1,6 +1,9 @@
 class Position(private val x: Int, private val y: Int) {
+    private val MIN: Int = 0
     init {
-        if (this.x < 0 || this.y < 0) {
+        val isNegativeInPosX = this.x < this.MIN
+        val isNegativeInPosY = this.y < this.MIN
+        if (isNegativeInPosX || isNegativeInPosY) {
             throw PositionOutOfBoundsException("Negative position not allowed")
         }
     }
