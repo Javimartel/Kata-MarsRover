6b06f0a8dfdaf70d907c488e9abf8f15f634494b
Javi <javiermespino@gmail.com>
2023-04-20 12:39:27 +0000

    Test(red): check if curiosity moves correctly

--- src/test/kotlin/RoverTest.kt
+++ src/test/kotlin/RoverTest.kt
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Assertions.*
 import kotlin.test.assertFailsWith
 class RoverTest {
 
-    private val initialPosition = Position(0, 0)
+    private val initialPosition = Position(1, 1)
     private val initialDirection = DIRECTION.NORTH
 
     private val curiosity: Rover = Rover(
@@ -17,5 +17,13 @@ class RoverTest {
             block = { curiosity.followThisOrders(listOf()) }
         )
         assertEquals(exception.message, "Any command received")
+    }
+
+    @Test
+    fun `check if curiosity moves correctly`() {
+        val commands = listOf(MOVE.FORWARD)
+        curiosity.followThisOrders(commands)
+
+        assertEquals(Position(0,1), curiosity.getCurrentPosition())
     }
 }
